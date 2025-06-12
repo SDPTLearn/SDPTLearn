388181efcc8ac77ef2e24acf144bd53273623451
NocturnJake <jarredexcel@gmail.com>
2025-06-12 21:58:23 +0800

    second commit

--- sdpt-learn/src/pages/Home.jsx
+++ sdpt-learn/src/pages/Home.jsx
@@ -2,11 +2,12 @@
 
 export function Home() {
   return (
-    <div className="home">
+    <div className="Home">
       <h1>Welcome to SDPT-Learn</h1>
       <p>Your journey to mastering software development starts here.</p>
       <p>Explore our resources, tutorials, and community support to enhance your skills.</p>
       <p>Join us in building a better future through technology!</p>
+      <h1>Tralalelo tralala</h1>
     </div>
   );
 }
